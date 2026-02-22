3645423f2477db2540d5b0ed4916f3bf10986219
FredRiver <yderfre7808@gmail.com>
2026-02-22 18:50:44 -0500

    style(main): chang

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
                 child: Text(
                   'Hola',
                   style: TextStyle(
-                    fontSize: 30,
+                    fontSize: 35,
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
                   ),
@@ -44,47 +44,99 @@ class MyApp extends StatelessWidget {
                   left: MediaQuery.of(context).size.width * 0.1,
                 ),
                 child: Text(
-                  'Logeate',
+                  'Logeate!',
                   style: TextStyle(
+                    fontSize: 25,
                     fontWeight: FontWeight.bold,
                     color: Colors.white,
+                  
                   ),
                 ),
               ),
 
-              const Text('Bienvenidos'),
-              const Text('Logeate para continuar'),
+              Spacer(),
               Container(
+                margin: EdgeInsets.only(bottom: 25),
                 padding: EdgeInsets.only(
                   top: MediaQuery.of(context).size.height * 0.1,
                   left: MediaQuery.of(context).size.width * 0.1,
                   right: MediaQuery.of(context).size.width * 0.1,
                 ),
                 height: MediaQuery.of(context).size.height * 0.7,
-                color: Colors.white,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                ),
                 child: Column(
                   children: [
                     TextFormField(
+                      
                       decoration: InputDecoration(
-                        border: OutlineInputBorder(),
                         hintText: 'Email',
+                        suffixIcon: Icon(Icons.email,
+                        color: Colors.blueAccent,
+                        ),
                       ),
                     ),
+                    SizedBox(height: 20),
                     TextFormField(
                       decoration: InputDecoration(
-                        border: OutlineInputBorder(),
                         hintText: 'Password',
+                        
+                        suffixIcon: Icon(Icons.lock,
+                         color: Colors.blueAccent,
+                        ),
+                        
+                      ),
+                      obscureText: true,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      alignment: Alignment.centerRight,
+                      child: Text('Olvidaste tu password?'),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.08,
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      margin: EdgeInsets.only(top: 30),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(255, 212, 219, 223),
+                            Colors.blueAccent,
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: FilledButton(
+                        onPressed: () {},
+                        style: FilledButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                        ),
+                        child: const Text(
+                          'INGRESAR',
+                          style: TextStyle(fontSize: 28),
+                        ),
                       ),
                     ),
-                    FilledButton(onPressed: () {}, child: const Text('Login')),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('no tiene Cuenta'),
-                        SizedBox(width: 30),
-                        Text('Registrate aqui'),
-                      ],
+                    Spacer(),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'No tiene Cuenta?',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.centerRight,
+                      margin: EdgeInsets.only(bottom: 30),
+                      child: Text(
+                        'Registrate ',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ],
                 ),
